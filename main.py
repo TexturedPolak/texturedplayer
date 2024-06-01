41e0f33218ee9af1a5d0455c2574ba4e6ae67e67
TexturedPolak <rafal200725@gmail.com>
2024-06-01 09:10:40 +0200

    Discord rpc support

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ import subprocess
 # Many things with os
 import os
 import signal
+# For discord RPC
+try:
+    from pypresence import Presence
+    import time
+    import multiprocessing
+    discordRPC_enabled = True
+except ModuleNotFoundError:
+    discordRPC_enabled = False
 
 # Init playlist and second process for playing music :)
 newplaylist = utils.get_newplaylist()
@@ -23,8 +31,35 @@ if os.name == "posix":
 else:
     proc = subprocess.Popen('echo TexturedPlayer for Windows', shell=True)
 
+# Enable discordRPC loop
+if discordRPC_enabled:
+    def init_discordRPC(state):
+        discordRPC = Presence('1246101303084585071') #Change if you want custom RPC ;)
+        def connect():
+            try:
+                discordRPC.connect()
+                return True
+            except:
+                return False
+        connected = connect()
+        while True:
+            if connected:
+                try:
+                    discordRPC.update(state=str(state.value), details="Playing",small_image='texturedplayer',small_text="TexturedPlayer")
+                except:
+                    connected = connect()
+            else:
+                connected = connect()
+            time.sleep(15)
+        
+    manager = multiprocessing.Manager()
+    current_song = manager.Value('Idle', 0)
+    discordRPC_loop=multiprocessing.Process(target=init_discordRPC, args=(current_song,))
+    discordRPC_loop.start()
+    current_song.value = "Idle"
 
-#Main Class
+
+# Main Class
 class TexturMusic(App):
     # Style
     CSS = """
@@ -69,10 +104,11 @@ class TexturMusic(App):
     def play_next_song(self):
         global proc
         global newplaylist
+        # Global discordRPC current song
+        if discordRPC_enabled:
+            global current_song
         poll = proc.poll()
         if poll is not None:
-            self.change_text("dead")
-            #thread = threading.Thread(target=musicplayer.play_song,args=(playlist[0],))
             if newplaylist["next"] <= len(newplaylist["playlist"]):
                 if os.name == "posix":
                     proc = subprocess.Popen(f'ffplay -nodisp -autoexit -af "volume=0.4" "{newplaylist["playlist"][newplaylist["next"]]}"',stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT, 
@@ -81,6 +117,9 @@ class TexturMusic(App):
                     proc = subprocess.Popen(f'ffplay -nodisp -autoexit -af "volume=0.4" "{newplaylist["playlist"][newplaylist["next"]]}"',stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT, 
                        shell=True)
                 self.change_text(str(utils.get_metadata(newplaylist["playlist"][newplaylist["next"]])))
+                # Change song in discord RPC (may display after 15 seconds)
+                if discordRPC_enabled:
+                    current_song.value = str(utils.get_metadata(newplaylist["playlist"][newplaylist["next"]]))
                 newplaylist["next"] += 1
                 utils.save_playlist(newplaylist)
             else:
@@ -124,6 +163,8 @@ class TexturMusic(App):
 if __name__ == "__main__":
     app = TexturMusic()
     app.run()
+    if discordRPC_enabled:
+        discordRPC_loop.terminate()
     if os.name == "posix":
         os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
     else:
